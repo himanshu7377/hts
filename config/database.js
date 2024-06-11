7d897888e0db838f260c254512e8ee623df80b91
himanshu7377 <him.singh7069@gmail.com>
2024-06-11 13:17:05 +0530

    sign-up

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, )
     .then(()=> console.log('MongoDB connected'))
     .catch(err => console.log(err));
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,3 +4,28 @@ const bcrypt = require('bcrypt');
 
 
 //  sign-up
+
+
+exports.signup = async(req,res)=>{
+
+    const {firstName, lastName, email, password} = req.body;
+
+    try {
+        //  hash the password before saving
+        const  hashedPassword = await bcrypt.hash(password, 10);
+
+       const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword
+       });
+    //     save user in database
+       await user.save();
+       res.status(201).send('User Registered Successfully');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(' Error :' + error.message);
+    }
+
+}
